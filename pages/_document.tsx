34423c3e57cf4d2ed9bed4c0b58a0166ffb7cc2a
Yunus Korkmaz <dev.yunuskorkmaz@gmail.com>
2023-01-23 17:11:41 +0300

    add pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon_xhdpi_96.png"/>
+        <meta name="theme-color" content="#fff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
